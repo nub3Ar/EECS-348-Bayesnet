1c39137df5b03225eb3c2d1390b8ae58ace96758
Yijing Zhang <yijingzhang2021@u.northwestern.edu>
2018-11-25 21:25:26 -0600

    Finished

--- student_code.py
+++ student_code.py
@@ -1,4 +1,28 @@
-
 def ask(var, value, evidence, bn):
-	pass
-				
+	hypo_dict = dict(evidence)
+	hypo_dict[var] = value
+	print(hypo_dict)
+	hypo_dict_f = dict(evidence)
+	hypo_dict_f[var] = not value
+	print(hypo_dict)
+	a = ask_recurse(list(bn.variables),hypo_dict, bn)
+	b = ask_recurse(list(bn.variables),hypo_dict_f, bn)
+	print(a, b)
+	return(a/(a+b))
+
+def ask_recurse(vars, values, bn):
+	#recursion is done when no more variables in the list
+	if (len(vars) == 0):
+		return 1
+	var = vars.pop(0)
+	if var.name in list(values):
+		prob = var.probability(values[var.name], values)
+		return prob * ask_recurse(vars,values,bn)
+	else:
+		values_t = dict(values)
+		values_t[var.name] = True
+		values_f = dict(values)
+		values_f[var.name] = False
+		var_t = list(vars)
+		var_f = list(vars)
+		return (var.probability(True, values)*ask_recurse(var_t, values_t, bn)+var.probability(False, values)*ask_recurse(var_f, values_f, bn))
